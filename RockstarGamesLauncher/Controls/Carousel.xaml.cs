485430b1f8c76831293c79299960a7c95a63b929
rohanharikr <rohanharikumar80@gmail.com>
2023-12-11 20:03:55 +0530

    update custom carousel control

--- RockstarGamesLauncher/Controls/Carousel.xaml.cs
+++ RockstarGamesLauncher/Controls/Carousel.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -18,11 +19,48 @@ namespace RockstarGamesLauncher.Controls
     /// <summary>
     /// Interaction logic for Slider.xaml
     /// </summary>
-    public partial class Slider : UserControl
+    public partial class Carousel : UserControl
     {
-        public Slider()
+        private int currentElement = 0;
+        private double cardWidth = 0f;
+        private double cardMargin = 0f;
+
+        public Carousel()
         {
             InitializeComponent();
+            Border card = CarouselStack.Children.OfType<Border>().First();
+            cardWidth = card.Width;
+            cardMargin = card.Margin.Right;
+        }
+
+        private void Left_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentElement < 4)
+            {
+                currentElement++;
+                AnimateCarousel();
+            }
+        }
+
+        private void Right_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentElement > 0)
+            {
+                currentElement--;
+                AnimateCarousel();
+            }
+        }
+
+        private void AnimateCarousel()
+        {
+            Storyboard storyboard = (this.Resources["CarouselStoryboard"] as Storyboard);
+            DoubleAnimation animation = storyboard.Children.First() as DoubleAnimation;
+            animation.Duration = TimeSpan.FromSeconds(0.2);
+            QuadraticEase easingFunction = new QuadraticEase();
+            easingFunction.EasingMode = EasingMode.EaseInOut;
+            animation.EasingFunction = easingFunction;
+            animation.To = -(cardWidth + cardMargin) * currentElement;
+            storyboard.Begin();
         }
     }
 }
