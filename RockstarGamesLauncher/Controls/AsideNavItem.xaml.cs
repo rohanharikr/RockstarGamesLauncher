e421779de2ad6094fc9cc3698dd5c6aaed0ce605
rohanharikr <rohanharikumar80@gmail.com>
2023-12-19 14:38:55 +0530

    game nav item

---
+++ RockstarGamesLauncher/Controls/AsideNavItem.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RockstarGamesLauncher.Controls
+{
+    public partial class AsideNavItem : UserControl
+    {
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", typeof(string), typeof(AsideNavItem), new PropertyMetadata(string.Empty));
+
+
+        public string Icon
+        {
+            get { return (string)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(string), typeof(AsideNavItem), new PropertyMetadata(string.Empty));
+
+
+        public AsideNavItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
