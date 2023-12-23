e49bd1e3b81a01ea7a8a50b346c024f1d350af07
rohanharikr <rohanharikumar80@gmail.com>
2023-12-23 20:21:27 +0530

    settings nav

--- RockstarGamesLauncher/ViewModels/SettingsViewModel.cs
+++ RockstarGamesLauncher/ViewModels/SettingsViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using RockstarGamesLauncher.Views.Settings;
@@ -12,30 +14,41 @@ namespace RockstarGamesLauncher.ViewModels
     partial class SettingsViewModel : ObservableObject
     {
         [ObservableProperty]
-        object currentPage = new SettingsGeneralView();
+        object? currentPage;
 
-        [RelayCommand]
-        void GotoGeneral()
+        private int _selectedPageIndex;
+        public int SelectedPageIndex
         {
-            CurrentPage = new SettingsGeneralView();
+            get { return _selectedPageIndex; }
+            set { _selectedPageIndex = value; Navigate(); }
         }
 
-        [RelayCommand]
-        void GotoAccountInfo()
+        public SettingsViewModel()
         {
-            CurrentPage = new SettingsAccountInfoView();
+            SelectedPageIndex = 0;
         }
 
-        [RelayCommand]
-        void GotoUpdates()
-        {
-            CurrentPage = new SettingsUpdatesView();
-        }
+        [ObservableProperty]
+        public List<string> settingsItems = new List<string> { "General", "Account information", "Updates", "Legal" };
 
         [RelayCommand]
-        void GotoLegal()
+        void Navigate()
         {
-            CurrentPage = new SettingsLegalView();
+            switch (SelectedPageIndex)
+            {
+                case 0:
+                    CurrentPage = new SettingsGeneralView();
+                    break;
+                case 1:
+                    CurrentPage = new SettingsAccountInfoView();
+                    break;
+                case 2:
+                    CurrentPage = new SettingsUpdatesView();
+                    break;
+                case 3:
+                    CurrentPage = new SettingsLegalView();
+                    break;
+            }
         }
     }
 }
